17c6cad341c577c10919979ce0f7a50e8031c53b
navin <navintb@gmail.com>
2020-01-14 01:41:27 +0530

    removed duplicate condition

--- src/main/java/com/codeseita/productsearchservice/model/util/Search.java
+++ src/main/java/com/codeseita/productsearchservice/model/util/Search.java
@@ -23,7 +23,7 @@ public class Search<T> {
     }
 
     public Specification<T> build() {
-        if(CollectionUtils.isEmpty(this.filters)) {
+        if (CollectionUtils.isEmpty(this.filters)) {
             return null;
         }
         List<Specification> specs = this.filters.stream()
@@ -38,49 +38,42 @@ public class Search<T> {
         return result;
     }
 
-    private Specification<T> getSpecification(Filter filter){
+    private Specification<T> getSpecification(Filter filter) {
         return (root, criteriaQuery, criteriaBuilder) -> getPredicate(filter, root, criteriaBuilder);
     }
 
     private Predicate getPredicate(Filter filter, Root<T> root, CriteriaBuilder builder) {
-        if(filter.getProperty() == null || filter.getValue() == null || filter.getOperation() == null) {
+        if (filter.getProperty() == null || filter.getValue() == null || filter.getOperation() == null) {
             return null;
         }
         if (filter.getOperation() == Filter.Operation.GREATER_THAN_OR_EQUAL_TO) {
             return builder.greaterThanOrEqualTo(
                     getExpression(root, filter), filter.getValue().toString());
-        }
-        else if (filter.getOperation() == Filter.Operation.LESS_THAN_OR_EQUAL_TO) {
+        } else if (filter.getOperation() == Filter.Operation.LESS_THAN_OR_EQUAL_TO) {
             return builder.lessThanOrEqualTo(
                     getExpression(root, filter), filter.getValue().toString());
-        }
-        else if (filter.getOperation() == Filter.Operation.LESS_THAN_OR_EQUAL_TO) {
-            return builder.lessThanOrEqualTo(
-                    getExpression(root, filter), filter.getValue().toString());
-        }
-        else if (filter.getOperation() == Filter.Operation.EQUALS) {
+        } else if (filter.getOperation() == Filter.Operation.EQUALS) {
             return builder.equal(
                     getExpression(root, filter), filter.getValue());
-        }
-        else if (filter.getOperation() == Filter.Operation.IN) {
-            if(filter.getValue() instanceof Collection) {
-                Object[] values = ((Collection)filter.getValue()).stream().toArray(Object[] :: new);
+        } else if (filter.getOperation() == Filter.Operation.IN) {
+            if (filter.getValue() instanceof Collection) {
+                Object[] values = ((Collection) filter.getValue()).stream().toArray(Object[]::new);
                 return getExpression(root, filter).in(values);
             }
         }
         return null;
     }
 
-    private Expression getExpression(Root<T> root, Filter filter){
+    private Expression getExpression(Root<T> root, Filter filter) {
         String[] joins = filter.getProperty().split("\\.");
-        if(joins.length == 1){
-            return root.<String> get(filter.getProperty());
+        if (joins.length == 1) {
+            return root.<String>get(filter.getProperty());
         }
         Join join = root.join(joins[0]);
-        for(int i=1;i<joins.length-1;i++){
+        for (int i = 1; i < joins.length - 1; i++) {
             join = join.join(joins[i]);
         }
-        return join.<String> get(joins[joins.length-1]);
+        return join.<String>get(joins[joins.length - 1]);
     }
 
 }
